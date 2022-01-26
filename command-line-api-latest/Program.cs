fc6f2b54b2ee97bdbdef6a9581edaf38c2492bb1
Adam Sitnik <adam.sitnik@gmail.com>
2022-01-26 19:59:25 +0100

    add latest System.System.CommandLine example

---
+++ command-line-api-latest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static void Run(bool boolean, string text)
+    {
+        Console.WriteLine($"Bool option: {text}");
+        Console.WriteLine($"String option: {boolean}");
+    }
+
+    private static Task<int> Main(string[] args)
+    {
+        Option<bool> boolOption = new Option<bool>(new[] { "--bool", "-b" }, "Bool option");
+        Option<string> stringOption = new Option<string>(new[] { "--string", "-s" }, "String option");
+
+        RootCommand command = new RootCommand
+        {
+            boolOption,
+            stringOption
+        };
+
+        command.SetHandler<bool, string>(Run, boolOption, stringOption);
+
+        return new CommandLineBuilder(command).Build().InvokeAsync(args);
+    }
+}
