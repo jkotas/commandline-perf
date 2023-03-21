1bf8dcf5ce935623d6f89ff2be078e1f08e2ff3a
Adam Sitnik <adam.sitnik@gmail.com>
2023-03-21 08:18:50 +0100

    update to version after all removals

--- command-line-api/now/Program.cs
+++ command-line-api/now/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.CommandLine;
-using System.CommandLine.Parsing;
 
 public class Program
 {
@@ -12,17 +11,17 @@ public class Program
 
     private static int Main(string[] args)
     {
-        Option<bool> boolOption = new Option<bool>(new[] { "--bool", "-b" }, "Bool option");
-        Option<string> stringOption = new Option<string>(new[] { "--string", "-s" }, "String option");
+        Option<bool> boolOption = new Option<bool>("--bool", "-b") { Description = "Bool option" };
+        Option<string> stringOption = new Option<string>("--string", "-s") { Description = "String option" };
 
-        RootCommand command = new RootCommand
+        RootCommand command = new ()
         {
             boolOption,
-            stringOption
+            stringOption,
         };
 
-        command.SetHandler<bool, string>(Run, boolOption, stringOption);
+        command.SetAction(ctx => Run(ctx.GetValue(boolOption), ctx.GetValue(stringOption)));
 
-        return new CommandLineBuilder(command).Build().Invoke(args);
+        return new CommandLineConfiguration(command).Invoke(args);
     }
 }
